7c7e29a9cb6498926e007cd19abba2507a2bb77f
garvitjain111 <yashjain12yj@gmail.com>
2018-01-05 00:47:05 +0530

    Map Implemented

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,11 @@ driverInfo = {
 	driverNo: 2
 }
 
-let lat=22.7248170, long=75.8243170, voltage=3, current=4, driverNo=1, speed=12, steeringAngle=50, tiltAngle=55, rpm=366, temp = 34, driverName = "Garvit";
+var lat=22.7248170, long=75.8243170, voltage=3, current=4, driverNo=1, speed=12, steeringAngle=50, tiltAngle=55, rpm=366, temp = 34, driverName = "Garvit";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   	res.render('index', { title: 'Acrostreak - A BAJA Project', lat, long, voltage, current, driverNo, speed, driverName, rpm, temp, tiltAngle, steeringAngle });
-});
-
-router.get('/data', function(req, res, next) {
-  	res.render('index', { title: 'Acrostreak - A BAJA Project', lat, long, voltage, current, driverNo, speed });
 });
 
 
